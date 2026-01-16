f6cb9821bc31a9c9ca7888826ae62b1f6a485560
Moh Bk <123780770+mohbkceo@users.noreply.github.com>
2026-01-16 21:25:30 +0100

    test Broswer routing n2

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
   <StrictMode>
+  <BrowserRouter>
     <App /> 
-  </StrictMode>,
   </BrowserRouter>
+  </StrictMode>,
 )
